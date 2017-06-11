dab32ccc8ca1b63b838433980a89c69e3bec68b1
Hamza <Hamza@cable-5-43-94-22.dynamic.telemach.ba>
2017-06-11 04:56:43 +0200

    initial commit after first

---
+++ js/my-app.js
@@ -0,0 +1,82 @@
+// Initialize your app
+var myApp = new Framework7({
+    precompileTemplates: true,
+    template7Pages: true, // need to set this
+    cache: false
+});
+ 
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+ 
+// Add view
+var mainView = myApp.addView('.view-main', {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
+});
+
+//Main ID for comedian
+var comedianID="df0273f966c42beb1c3f47532df4432c514b04b2484239f277e5dc5acf680ab2";
+
+var isAjaxLoaded=false;
+var pathToAjaxDispatcher="http://www.ihumoru.com/appengine/php/ajaxDispatcher.php";
+
+function handleWithMainWrapper(div){
+    if(isAjaxLoaded) return false;
+    isAjaxLoaded=true;
+    var postData={id: comedianID, context: "loadMainAPPWrapper"};
+    
+    $$.ajax({
+       type: "POST",
+       url: pathToAjaxDispatcher,
+       data: postData,
+       dataType: "json",
+       success: function(data){
+           isAjaxLoaded=false;
+               if(data["success"]==1){
+                   //Load custom stylesheet and embeed it in the head
+                   for(var i in data["css"]){
+                       var cssItem=data["css"][i];
+                       $$('<link rel="stylesheet" type="text/css" href="'+cssItem+'" />').appendTo("head");
+                   }
+                   
+                   //Load custom stylesheet and embeed it in the head
+                   for(var i in data["js"]){
+                       var jsItem=data["js"][i];
+                       
+                       JavaScript.load(jsItem, function() {
+                           dinyamicUseScript();
+                           //Timeout to load the content and properly display it
+                           div.html(data["content"]);
+                      });
+                   }
+                   
+                   
+               }
+       }, error: function(){
+           isAjaxLoaded=false;
+       }
+    });
+}
+
+if($$("#mainWrapper").length>0){
+    //APP loads dynamic content
+    handleWithMainWrapper($$("#mainWrapper"));
+}
+
+
+var JavaScript = {
+  load: function(src, callback) {
+    var script = document.createElement('script'),
+        loaded;
+    script.setAttribute('src', src);
+    if (callback) {
+      script.onreadystatechange = script.onload = function() {
+        if (!loaded) {
+          callback();
+        }
+        loaded = true;
+      };
+    }
+    document.getElementsByTagName('body')[0].appendChild(script);
+  }
+};
